fac2dbe06405a97661c1ad8fb62327763860cbbe
Alexander Obregon <alexander.obregon47@gmail.com>
2022-02-24 12:10:29 -0800

    commit tree cf258297208625178acfe222e4976bbd2926a2de

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,7 @@
 class Taxi < ApplicationRecord
+    has_many :rides
+    has_many :passengers, through: :rides
 end
+
+
+# rails generate migration AddIdsToRides passenger_id:integer taxi_id:integer
